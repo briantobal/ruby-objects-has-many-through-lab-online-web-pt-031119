2ae6d6d59c2d9bb33f415a103d6b347835bda8cd
Brian Tobal <brian.tobal@gmail.com>
2019-04-21 21:04:34 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -2,6 +2,17 @@ class Patient
 attr_accessor 
 @@all = []
 
+def self.all 
+  @@all 
+  end     #def 
 
+def initialized(name)
+  @name = name 
+  @@all << self 
+  end     #def 
+
+def new_appointment(doctor, date)
+  Appointment.new(doctor, self, date)
+  end     #def 
 
 end 
