0214923ae44d5a6a1575d67c9fc6f61057d2c28d
Brian Tobal <brian.tobal@gmail.com>
2019-04-21 20:40:15 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,29 +1,26 @@
-class Artist 
-  attr_accessor :name 
+class Artist
+  attr_accessor :name
+
   @@all = []
-  
+
   def initialize(name)
     @name = name
-    @@all << self 
-  end 
-  
-  def self.all 
+    @@all << self
+  end
+
+  def self.all
     @@all
-  end 
-  
+  end
+
   def new_song(name, genre)
     Song.new(name, self, genre)
-  end     #def 
-  
-  def songs 
-    Song.all.select {|song| song.name == self}  
-  end     #def 
-  
-  def genres 
-    #self.songs.collect do |song|
-    #  song.genre
-    #end 
-     songs.map(&:genre)
-  end     #def 
-  
-end 
+  end
+
+  def songs
+    Song.all.select { |song| song.artist == self }
+  end
+
+  def genres
+    songs.map(&:genre)
+  end
+end
